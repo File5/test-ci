cd97cb83909bc35976fe35dc87516fd8dd0d68b7
Alexander Zuev <zuev08@gmail.com>
2017-01-06 16:42:33 +0300

    New feature

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Gson gson = new Gson();
         System.out.println("Test Gradle project");
+        System.out.println("New feature!");
     }
 
     public int sum(int a, int b) {
