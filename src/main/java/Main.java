2378398dd7130863acc8c09b2bd69f9ec80b3466
Alexander Zuev <zuev08@gmail.com>
2017-01-06 16:05:30 +0300

    Init project

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.google.gson.Gson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        System.out.println("Test Gradle project");
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    private Main main;
+
+    @Before
+    public void init() {
+        main = new Main();
+    }
+
+    @Test
+    public void testSum_always_returnValidValue() throws Exception {
+        int a = 5;
+        int b = 9;
+        int expected = a + b;
+
+        int actual = main.sum(a, b);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_always_fails() throws Exception {
+        assertTrue(false);
+    }
+
+}
